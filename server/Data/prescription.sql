6fae85ec23c541d668bd85ab63674a2585805f26
zhangbaoxi <396498984@qq.com>
2016-12-07 18:41:25 +0800

    设计表
    修改了药和方的表结构

--- server/Data/prescription.sql
+++ server/Data/prescription.sql
@@ -1,6 +1,5 @@
-`tag` VARCHAR(200) COMMENT '标签',
+﻿`tag` VARCHAR(200) COMMENT '标签',
 `name` VARCHAR(45) COMMENT '方名', 
 `book` VARCHAR(45) COMMENT '书名（出处），书名为《伤寒论》的称为经方，出自其他书籍的为非经方', 
 `summary` text COMMENT '概述', 
 `indications` text COMMENT '主治', 
-`explanation` text COMMENT '方义方解', 
