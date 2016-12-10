4ca706f316759a232075452314325600dc3428e6
hucaihua <13683518428@163.com>
2016-12-10 14:56:46 +0800

    add hottag interface

--- client/app/src/main/java/com/cmssp/module/cmsense/FTSFragment.java
+++ client/app/src/main/java/com/cmssp/module/cmsense/FTSFragment.java
@@ -2,6 +2,7 @@ package com.cmssp.module.cmsense;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.cmssp.R;
@@ -17,6 +18,9 @@ public class FTSFragment extends BaseFragment{
     @BindView(R.id.title)
     TextView title;
 
+    @BindView(R.id.FTSEditText)
+    EditText editText;
+
     @Override
     protected int getLayout() {
         return R.layout.layout_fragment_cmfts;
@@ -25,5 +29,6 @@ public class FTSFragment extends BaseFragment{
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         title.setText(R.string.app_name);
+
     }
 }
--- server/Cmssp/Api/FTS.php
+++ server/Cmssp/Api/FTS.php
@@ -9,7 +9,7 @@ class Api_FTS extends PhalApi_Api {
     public function getRules() {
         return array(
             'load' => array(
-				
+
             ),
         );
     }
@@ -24,11 +24,25 @@ class Api_FTS extends PhalApi_Api {
      */
     public function load() {
         $rs = array();
-		
+
 		//fetch data
         $domain = new Domain_FTS();
         $rs['FTS'] = $domain->loadFTS();
 		// return
         return $rs;
+    }
+
+    /**
+     * hotTag
+     * @desc 获取热门疾病标签
+    * @return ret 返回码 类似http请求 2XX 4XX 5XX
+    * @return msg 请求描述
+    * @return data 实际返回数据
+    * @return array 疾病标签列表
+     */
+    public function hotTag(){
+      $rs = array("感冒","发烧","便秘","肚子痛","咳嗽","腰痛","关节炎","头晕","头痛","头胀","四肢麻木","手脚冰冷");
+
+      return $rs;
     }
 }
